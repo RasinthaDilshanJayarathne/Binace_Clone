8a0570d966401edcc4ad7b500f278a02fa130f18
Radintha Dilshan <rasinthadilshan9@gmail.com>
2022-08-21 06:42:16 +0530

    Creating File flow

---
+++ src/app/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HeadeComponents from '../components/HeadeComponents';
+
+function App() {
+  return (
+    <div>
+        <HeadeComponents />
+    </div>
+  );
+}
+
+export default App;
