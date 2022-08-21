074240f44f718178525eed1bc47df5ca1affe801
Radintha Dilshan <rasinthadilshan9@gmail.com>
2022-08-21 07:46:38 +0530

    Create Blog Component

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import HeadeComponents from '../components/HeadeComponents';
 import CoverComponent from '../components/CoverComponent';
+import BlogComponent from '../components/BlogComponent';
 
 function App() {
   return (
     <div>
         <HeadeComponents />
         <CoverComponent />
+        <BlogComponent />
     </div>
   );
 }
