0702bbf9358d61feb0e3eabf985ebc887238a07f
Radintha Dilshan <rasinthadilshan9@gmail.com>
2022-08-21 07:06:42 +0530

    Create Cover Component

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import HeadeComponents from '../components/HeadeComponents';
+import CoverComponent from '../components/CoverComponent';
 
 function App() {
   return (
     <div>
         <HeadeComponents />
+        <CoverComponent />
     </div>
   );
 }
---
+++ src/components/CoverComponent.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function CoverComponent() {
+    return (
+        <div className='Cover'>
+            <div className='cover_top'>
+                <h1>Buy Bitcoin with INR</h1>
+                <p>Join the world's largest crypto exchange. Designed for India</p>
+                <button className="yellow_button">Register Now</button>
+            </div>
+
+            <div className='cover_middle'>
+
+            </div>
+
+            <div className='cover_bottom'>
+
+            </div>
+        </div>
+    )
+}
