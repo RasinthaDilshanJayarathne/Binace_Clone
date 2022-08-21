5354ddb9a6699514baec28b036c50cc8d60bb06b
Radintha Dilshan <rasinthadilshan9@gmail.com>
2022-08-21 07:01:19 +0530

    Create Header Bar

--- src/components/HeadeComponents.js
+++ src/components/HeadeComponents.js
@@ -7,8 +7,12 @@ export default function HeadeComponents() {
         <div className='header'>
             <img src={logo} alt="logo" />
 
-            <div className='right_header'>
+            <div className='right_header header'>
+                <button className="grey_button yellow_button">Login</button>
+                <button className="yellow_button ">Register</button>
                 <h3>Downloads</h3>
+                <h3>English(India)</h3>
+                <h3>USD</h3>
             </div>
         </div>
     )
