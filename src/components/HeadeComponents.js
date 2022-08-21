0e44c58e383e93079eef18427050a1e4dab57072
Radintha Dilshan <rasinthadilshan9@gmail.com>
2022-08-21 06:49:13 +0530

    Add Header styles

--- src/components/HeadeComponents.js
+++ src/components/HeadeComponents.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from 'react';
+import logo from "../assets/logo.png";
+import "../app/App.css";
 
 export default function HeadeComponents() {
-  return (
-    <div></div>
-  )
+    return (
+        <div className='header'>
+            <img src={logo} alt="logo" />
+
+            <div className='right_header'>
+                <h3>Downloads</h3>
+            </div>
+        </div>
+    )
 }
